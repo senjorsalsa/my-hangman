8f264c22a88c945d899b9150126331faf21b9ffd
Victor <67063010+senjorsalsa@users.noreply.github.com>
2025-01-21 21:09:09 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,169 @@
+/* eftersom det bara är ett litet projekt till min sambo orkar jag inte dölja orden ^^ */
+const hangmanWords = [
+  "äpple",
+  "banan",
+  "boken",
+  "bordet",
+  "stolen",
+  "skolan",
+  "lärarens",
+  "elever",
+  "klassrum",
+  "snöboll",
+  "blomma",
+  "trädgård",
+  "floden",
+  "stranden",
+  "bergen",
+  "dalgång",
+  "sandig",
+  "sjöarna",
+  "flygplan",
+  "kameran",
+  "fiskar",
+  "choklad",
+  "kakan",
+  "hamburgare",
+  "pastan",
+  "grönsak",
+  "marmelad",
+  "kläder",
+  "jackan",
+  "tröjor",
+  "byxorna",
+  "strumpor",
+  "handskar",
+  "halsduk",
+  "paraply",
+  "resorna",
+  "jobbet",
+  "semester",
+  "flygplats",
+  "motorcykel",
+  "familjen",
+  "vänskap",
+  "kärleken",
+  "historisk",
+  "framtiden",
+  "nutiden",
+  "glädjen",
+  "sorgen",
+  "väderlek",
+  "klockan",
+  "morgonen",
+  "kvällen",
+  "veckorna",
+  "decennier",
+  "sekler",
+  "arbete",
+  "kollegor",
+  "mötena",
+  "betygen",
+  "examen",
+  "språken",
+  "bokstäver",
+  "nummer",
+  "filmer",
+  "musikens",
+  "radioapparat",
+  "strandkant",
+  "vintertid",
+  "sommarkväll",
+  "höstmorgon",
+  "trädstam",
+  "skogsväg",
+  "cykling",
+  "parkerad",
+  "blommor",
+  "sjöutsikt",
+  "solstrålar",
+  "månsken",
+  "stjärnklart",
+  "fjärilar",
+  "fågelsång",
+  "skogsglänta",
+  "strandnära",
+  "bergstopp",
+  "flodhäst",
+  "fåglarna",
+  "skidbacke",
+  "skidåkning",
+  "båtresa",
+  "utflykten",
+];
+
+let buttons = document.querySelectorAll(".letterButton");
+
+for (let button of buttons) {
+  button.addEventListener("click", roundLogic);
+}
+
+const getRandomWord = () =>
+  hangmanWords[Math.floor(Math.random() * hangmanWords.length)];
+
+let remainingGuesses = 6;
+let randomWord = getRandomWord().toUpperCase();
+let theWord = "";
+
+for (let i = 0; i < randomWord.length; i++) {
+  theWord += "_";
+}
+
+const newGameButton = document.querySelector(".new-game");
+newGameButton.addEventListener("click", newGame);
+const correct = document.querySelector(".correct");
+correctSpan = document.querySelector("#cWord");
+const image = document.querySelector("img");
+const remaining = document.querySelector(".remaining span");
+let guessingWordElement = document.querySelector(".word p");
+guessingWordElement.innerText = theWord;
+
+function roundLogic() {
+  let letter = this.innerText;
+  this.disabled = true;
+  if (randomWord.includes(letter)) {
+    let newWord = "";
+    for (let i = 0; i < randomWord.length; i++) {
+      if (theWord[i] === "_" && randomWord[i] === letter) {
+        newWord += letter;
+      } else if (theWord[i] === "_" && randomWord[i] !== letter) {
+        newWord += "_";
+      } else {
+        newWord += theWord[i];
+      }
+    }
+    theWord = newWord;
+    guessingWordElement.innerText = newWord;
+  } else {
+    remainingGuesses--;
+    remaining.innerText = remainingGuesses;
+    image.src = `img/stage${remainingGuesses}.jpg`;
+    if (remainingGuesses === 0) {
+      /* Player lost */
+      for (let button of buttons) {
+        button.disabled = true;
+      }
+      correctSpan.innerText = randomWord;
+      correct.style.display = "block";
+    }
+  }
+  if (!theWord.includes("_")) {
+    /* Player won */
+    correct.innerHTML = "<p>Congratulations! You won!</p>";
+  }
+}
+
+function newGame() {
+  theWord = "";
+  for (let button of buttons) {
+    button.disabled = false;
+  }
+  randomWord = getRandomWord().toUpperCase();
+  for (let i = 0; i < randomWord.length; i++) {
+    theWord += "_";
+  }
+  guessingWordElement.innerText = theWord;
+  remainingGuesses = 6;
+  remaining.innerText = remainingGuesses;
+  image.src = `img/stage6.jpg`;
+}
